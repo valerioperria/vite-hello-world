7ab44040ffb809bdf07af5b6bcd0d3c27c92ed82
valerioperria <valerio.perria@gmail.com>
2023-11-22 16:57:21 +0100

    feat: create AppTitle.vue and create a title

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script>
-
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle,
+    AppTitle
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
 </script>
 
 <template>
- 
+  <AppTitle />
 </template>
 
-<style scoped>
 
-</style>
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style></style>
